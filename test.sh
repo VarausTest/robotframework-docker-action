3861b4d79c9c7fba1e02ec83e615379bbe252cbe
VarausTest <qfaksi@gmail.com>
2023-10-16 15:01:14 +0300

    Added timezone configuration to Docker run command

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e BROWSER=$BROWSER \
+  -e TZ=Europe/Helsinki \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
